80110e96fb72c1a84c78d6d12fec1447ceca1557
Autobeveiliging Nederland <db.ecommerce2019@gmail.com>
2025-11-05 17:28:31 +0100

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -88,7 +88,7 @@ export default function Footer() {
             <ul className="space-y-2 text-gray-400">
               <li>Kiwa SCM Gecertificeerd</li>
               <li>CCV Voertuigbeveiliging</li>
-              <li>Rtregistratiesystemen</li>
+              <li>Ritregistratiesystemen</li>
             </ul>
           </div>
         </div>
